ec4f6001a3f7a7faac2a42da9ab0e1a128637ec1
NikkoUrza <nikkourza@outlook.com>
2026-05-27 21:24:33 -0500

    presskit

--- api/presskit.js
+++ api/presskit.js
@@ -72,7 +72,7 @@ module.exports = async (req, res) => {
             hero: 'Images/HomePortada.png',
             foto: 'Images/PressKit.png',
             quote: 'Soy ese artista que quiere ser dueño de su arte y de su tiempo, que quiere crear con libertad, que no se encasilla en un género porque la música es infinita.',
-            bio: 'Nikko Sepúlveda es un productor, compositor e intérprete de música urbana caleño, es un apasionado y creativo que cuenta con 10 años de experiencia en producción y composición, ha tenido la oportunidad de trabajar con diversos artistas de la escena en la ciudad de Cali.',
+            bio: 'Nikko Urza es un productor, compositor e intérprete de música urbana caleño, es un apasionado y creativo que cuenta con 10 años de experiencia en producción y composición, ha tenido la oportunidad de trabajar con diversos artistas de la escena en la ciudad de Cali.',
             tags: 'Afrobeat, Rap, Reggae, Trap, Lo-fi, Colombia, Independiente',
             anios: '12+',
             generos: '∞'
